76e8ce46d9c4abf102d1810c4a8023dbd1cd5ba5
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-03-20 17:23:06 -0400

    InfectionRate calculator done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,70 @@
 import PageLayout from "../layouts/Page";
+import Slider from "@material-ui/core/Slider";
+import TextField from "@material-ui/core/TextField";
+import { useState } from "react";
 
-const IndexPage = () => (
-  <PageLayout>
+const InfectionRate = ({ population, setPopulation }) => {
+  const [percent, setPercent] = useState(30);
+  const [rate, setRate] = useState(10);
+  const [current, setCurrent] = useState(0);
+
+  const percentInfected = (population * percent) / 100;
+  const days = Math.round(
+    (Math.log(percentInfected) - Math.log(current)) / Math.log(1 + rate / 100)
+  );
+
+  return (
     <div>
-      <h1>Coronavirus Testing</h1>
+      <h2 className="text-lg">Infection Rates</h2>
+      {population}
+      <div>
+        <form>
+          <TextField
+            label="Total Population"
+            variant="filled"
+            value={population}
+            type="number"
+            onChange={x => setPopulation(x.target.value)}
+          />
+          {percent}
+          <Slider
+            value={percent}
+            step={5}
+            onChange={(_, value) => setPercent(value)}
+          />
+
+          <p>{percentInfected}</p>
+
+          {rate}
+          <Slider value={rate} onChange={(_, value) => setRate(value)} />
+
+          <TextField
+            label="Currently Infected"
+            variant="filled"
+            value={current}
+            type="number"
+            onChange={x => setCurrent(x.target.value)}
+          />
+
+          {days}
+        </form>
+      </div>
     </div>
-  </PageLayout>
-);
+  );
+};
+
+const IndexPage = () => {
+  const [population, setPopulation] = useState(100000);
+
+  return (
+    <PageLayout>
+      <div>
+        <h1 className="text-xl">Coronavirus Testing</h1>
+
+        <InfectionRate population={population} setPopulation={setPopulation} />
+      </div>
+    </PageLayout>
+  );
+};
 
 export default IndexPage;
