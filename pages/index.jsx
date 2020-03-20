679f3734c66e8c202b08305de51cf877ea790e8f
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-03-20 16:32:29 -0400

    IndexPage created

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import PageLayout from "../layouts/Page";
+
+const IndexPage = () => (
+  <PageLayout>
+    <div>
+      <h1>Coronavirus Testing</h1>
+    </div>
+  </PageLayout>
+);
+
+export default IndexPage;
