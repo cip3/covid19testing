26bf1b04ec2842c54f934ff265e34457cb09943f
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-03-20 16:18:57 -0400

    Page Layout done

---
+++ layouts/Page.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const PageLayout = ({
+  title = "Covid 19 Testing",
+  description = "Covid 19 Testing",
+  children
+}) => (
+  <>
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"
+      />
+    </Head>
+
+    <div className="antialiased min-h-screen">
+      <div>
+        <Header />
+
+        {children}
+
+        <Footer />
+      </div>
+    </div>
+
+    <style jsx global>{`
+      body {
+        line-height: 1.6;
+      }
+    `}</style>
+  </>
+);
+
+export default PageLayout;
