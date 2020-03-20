98af12e382d5798c8a6f8b84113e9e286f40376b
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-03-20 17:45:33 -0400

    TestingRate done

--- pages/index.jsx
+++ pages/index.jsx
@@ -53,6 +53,54 @@ const InfectionRate = ({ population, setPopulation }) => {
   );
 };
 
+const TestingRate = ({ population, setPopulation }) => {
+  const [percent, setPercent] = useState(30);
+  const [tests, setTests] = useState(1000);
+
+  const totalTests = (population * percent) / 100;
+
+  const days = Math.round(totalTests / tests);
+
+  return (
+    <div>
+      <h2 className="text-lg">Infection Rates</h2>
+      {population}
+      <div>
+        <form>
+          <TextField
+            label="Total Population"
+            variant="filled"
+            value={population}
+            type="number"
+            onChange={x => setPopulation(x.target.value)}
+          />
+          {percent}
+          <Slider
+            value={percent}
+            step={5}
+            min={0}
+            max={200}
+            onChange={(_, value) => setPercent(value)}
+          />
+
+          {tests}
+          <Slider
+            value={tests}
+            step={1000}
+            min={1000}
+            max={300000}
+            onChange={(_, value) => setTests(value)}
+          />
+
+          <p>{totalTests}</p>
+
+          {days}
+        </form>
+      </div>
+    </div>
+  );
+};
+
 const IndexPage = () => {
   const [population, setPopulation] = useState(100000);
 
@@ -62,6 +110,7 @@ const IndexPage = () => {
         <h1 className="text-xl">Coronavirus Testing</h1>
 
         <InfectionRate population={population} setPopulation={setPopulation} />
+        <TestingRate population={population} setPopulation={setPopulation} />
       </div>
     </PageLayout>
   );
