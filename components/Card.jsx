3921b7f6a5a6e8a34eb7dbd81c7ac90cd9260ec6
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-03-20 18:02:27 -0400

    Spacing improved

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Card = ({ children }) => (
-  <div className="p-4 md:p-8 rounded shadow bg-white">{children}</div>
+  <div className="p-4 md:py-2 md:px-8 md:py-4 rounded shadow bg-white">
+    {children}
+  </div>
 );
 
 export default Card;
 
 export const CardContent = ({ children }) => (
-  <div className="my-4">{children}</div>
+  <div className="my-2 md:my-4">{children}</div>
 );
 
 export const CardHeading = ({ children }) => (
